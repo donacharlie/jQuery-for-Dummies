37cd16ac4f906053a5f5437898def3adfcec8dd0
donacharlie <donacharlie@yahoo.fr>
2017-04-10 19:04:37 +0200

    Delete js.js

--- ExDavid3/js.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-
-        $("#btnSubmit").click(function(){
-                $.post('test.php',
-                      $("#testform").serialize();
-                );
-        });
-                
-});
