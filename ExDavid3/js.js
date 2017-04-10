d75618be0834ce946bad8f473b78aaa4eae2da80
donacharlie <donacharlie@yahoo.fr>
2017-04-10 18:41:13 +0200

    Add files via upload

---
+++ ExDavid3/js.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+        $("#btnSubmit").click(function(){
+    
+            $("#testform").load('https://donacharlie.github.io/jQuery-for-Dummies//ExDavid3/mywebpage.html' #email #fname #lname')
+
+
+        });
+                
+});
